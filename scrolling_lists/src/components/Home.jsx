ed01ac1092f3c7bd0227760891ab661350e7286d
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-11-05 17:47:08 +0530

    authentication part done

--- scrolling_lists/src/components/Home.jsx
+++ scrolling_lists/src/components/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './style.css'
 
 export const Home = () => {
+ 
   return (
-    <div>Home</div>
+    <div>
+      <div className='nav'>
+        <div><img src="https://www.shutterstock.com/image-vector/welcome-lettering-sign-handwritten-modern-600w-1452185639.jpg" alt="" width="250px" height="55px"/></div>
+        <div><button className='btn'>Logout</button></div>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
